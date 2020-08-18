fcaf71058c7c6fe8fdab8fbb59c70f73f2f2f9e1
Directoire <efehandemirzeist@gmail.com>
2020-08-19 00:32:00 +0200

    Initial commit

--- Template/Modules/ExampleModule.cs
+++ Template/Modules/ExampleModule.cs
@@ -1,7 +1,39 @@
-﻿namespace Template.Modules
+﻿using System.Data;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace Template.Modules
 {
-    public class ExampleModule
+    public class ExampleModule : ModuleBase<SocketCommandContext>
     {
-        
+        private readonly ILogger<ExampleModule> _logger;
+
+        public ExampleModule(ILogger<ExampleModule> logger)
+            => _logger = logger;
+
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong!");
+            _logger.LogInformation($"{Context.User.Username} executed the ping command!");
+        }
+
+        [Command("echo")]
+        public async Task EchoAsync([Remainder] string text)
+        {
+            await ReplyAsync(text);
+            _logger.LogInformation($"{Context.User.Username} executed the echo command!");
+        }
+
+        [Command("math")]
+        public async Task MathAsync([Remainder] string math)
+        {
+            var dt = new DataTable();
+            var result = dt.Compute(math, null);
+            
+            await ReplyAsync($"Result: {result}");
+            _logger.LogInformation($"{Context.User.Username} executed the math command!");
+        }
     }
 }
--- Template/Services/CommandHandler.cs
+++ Template/Services/CommandHandler.cs
@@ -1,7 +1,52 @@
-﻿namespace Template.Services
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Template.Services
 {
-    public class CommandHandler
+    public class CommandHandler : InitializedService
     {
-        
+        private readonly IServiceProvider _provider;
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _service;
+        private readonly IConfiguration _config;
+
+        public CommandHandler(IServiceProvider provider, DiscordSocketClient client, CommandService service, IConfiguration config)
+        {
+            _provider = provider;
+            _client = client;
+            _service = service;
+            _config = config;
+        }
+
+        public override async Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            _client.MessageReceived += OnMessageReceived;
+            _service.CommandExecuted += OnCommandExecuted;
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+
+        private async Task OnMessageReceived(SocketMessage arg)
+        {
+            if (!(arg is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            var argPos = 0;
+            if (!message.HasStringPrefix(_config["prefix"], ref argPos) && !message.HasMentionPrefix(_client.CurrentUser, ref argPos)) return;
+
+            var context = new SocketCommandContext(_client, message);
+            await _service.ExecuteAsync(context, argPos, _provider);
+        }
+
+        private async Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            if (command.IsSpecified && !result.IsSuccess) await context.Channel.SendMessageAsync($"Error: {result}");
+        }
     }
 }
